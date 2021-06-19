e49996aa210a6a9c7f6631207c311c521853e728
PadmavathiSirangu <padmavathisirangu2001@gmail.com>
2021-06-19 19:55:13 +0530

    insert

--- insert.sql
+++ insert.sql
@@ -37,7 +37,8 @@ insert into books_subjects(book,subject) values (5,3);
 insert into books_subjects(book,subject) values (5,6);
 insert into books_subjects(book,subject) values (6,6);
 insert into books_subjects(book,subject) values (6,7);
+insert into books_subjects(book,subject) values (7,5);
 insert into books_subjects(book,subject) values (7,8);
-insert into books_subjects(book,subject) values (7,9);
+insert into books_subjects(book,subject) values (8,8);
 insert into books_subjects(book,subject) values (8,9);
-insert into books_subjects(book,subject) values (8,10);
+
