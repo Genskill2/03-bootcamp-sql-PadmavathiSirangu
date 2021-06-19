c64560cb83ad84f05b4eef8ea293135d7b5fe834
PadmavathiSirangu <padmavathisirangu2001@gmail.com>
2021-06-19 19:21:47 +0530

    insert error fixed

--- insert.sql
+++ insert.sql
@@ -6,7 +6,7 @@ insert into publisher(name,country) values("Del Rey","UK");
 insert into publisher(name,country) values("Vintage","UK");
 
 insert into books(title,publisher) values ("The C Programming Language",1);
-insert into books(title,publisher) values ("The Go Programming Language ",1);
+insert into books(title,publisher) values ("The Go Programming Language",1);
 insert into books(title,publisher) values ("The UNIX Programming Environment",1);
 insert into books(title,publisher) values ("Cryptonomicon",2);
 insert into books(title,publisher) values ("Deep Work",3);
@@ -37,7 +37,7 @@ insert into books_subjects(book,subject) values (5,3);
 insert into books_subjects(book,subject) values (5,6);
 insert into books_subjects(book,subject) values (6,6);
 insert into books_subjects(book,subject) values (6,7);
-insert into books_subjects(book,subject) values (7,5);
 insert into books_subjects(book,subject) values (7,8);
-insert into books_subjects(book,subject) values (8,8);
+insert into books_subjects(book,subject) values (7,9);
 insert into books_subjects(book,subject) values (8,9);
+insert into books_subjects(book,subject) values (8,10);
